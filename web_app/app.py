5e02ba1254206551745e04d15f6a3d9c37ade665
Rüdiger Küpper <ruediger@kuepper.nrw>
2018-04-25 20:05:17 +0200

    add port

--- web_app/app.py
+++ web_app/app.py
@@ -113,7 +113,7 @@ def delete_task(task_id):
     return jsonify( { 'result': True } )
     
 if __name__ == '__main__':
-    app.run(debug = True, host='0.0.0.0')
+    app.run(debug = True, host='0.0.0.0', port=5000)
     if 'description' in request.json and type(request.json['description']) is not str:
         abort(400)
 
